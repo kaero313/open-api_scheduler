a8d01196377bcc722e48035d7369b1648ea21fb9
HOME <kaero313@naver.com>
2024-08-06 17:27:26 +0900

    프로젝트 기본 세팅  - maven  - quartz-scheduler

---
+++ src/main/java/api/weatherController.java
@@ -0,0 +1,25 @@
+package api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@Service("WeatherController")
+public class weatherController {
+
+    public void getWeather() {
+
+
+    }
+
+}
