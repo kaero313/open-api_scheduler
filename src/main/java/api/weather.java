506c23e961c2babbf407b1395a31859d2c61c9a7
HOME <kaero313@naver.com>
2024-08-07 17:52:45 +0900

    api를 url connection하여 받아온 데이터를 파싱한 후 출력하는 기능 추가

--- src/main/java/api/weather.java
+++ src/main/java/api/weather.java
@@ -4,31 +4,15 @@ public class weather {
 
     private int coordX;
     private int coordY;
-    private String fcastYmd;
-    private String fcastTime;
-    private String wxType;
     private String baseYmd;
     private String baseTime;
     private String rainProbabl;
     private String rainType;
     private String humidity;
-    private String skyType;
     private String curTemp;
-    private String lwstTemp;
-    private String hgstTemp;
     private String windType;
     private String wind;
-    private String skyYmd;
-    private String skyTime;
-
-    private String areaCd;
     private String areaNm;
-    private String warngVarType;
-    private String warngStressType;
-    private String commandType;
-    private String startTime;
-    private String startTimeMd;
-    private String startTimeHm;
 
     private int deviceSnsrId;
 
@@ -46,30 +30,6 @@ public class weather {
 
     public void setCoordY(int coordY) {
         this.coordY = coordY;
-    }
-
-    public String getFcastYmd() {
-        return fcastYmd;
-    }
-
-    public void setFcastYmd(String fcastYmd) {
-        this.fcastYmd = fcastYmd;
-    }
-
-    public String getFcastTime() {
-        return fcastTime;
-    }
-
-    public void setFcastTime(String fcastTime) {
-        this.fcastTime = fcastTime;
-    }
-
-    public String getWxType() {
-        return wxType;
-    }
-
-    public void setWxType(String wxType) {
-        this.wxType = wxType;
     }
 
     public String getBaseYmd() {
@@ -112,36 +72,12 @@ public class weather {
         this.humidity = humidity;
     }
 
-    public String getSkyType() {
-        return skyType;
-    }
-
-    public void setSkyType(String skyType) {
-        this.skyType = skyType;
-    }
-
     public String getCurTemp() {
         return curTemp;
     }
 
     public void setCurTemp(String curTemp) {
         this.curTemp = curTemp;
-    }
-
-    public String getLwstTemp() {
-        return lwstTemp;
-    }
-
-    public void setLwstTemp(String lwstTemp) {
-        this.lwstTemp = lwstTemp;
-    }
-
-    public String getHgstTemp() {
-        return hgstTemp;
-    }
-
-    public void setHgstTemp(String hgstTemp) {
-        this.hgstTemp = hgstTemp;
     }
 
     public String getWindType() {
@@ -160,54 +96,6 @@ public class weather {
         this.wind = wind;
     }
 
-    public String getSkyYmd() { return skyYmd; }
-
-    public void setSkyYmd(String skyYmd) { this.skyYmd = skyYmd; }
-
-    public String getSkyTime() { return skyTime; }
-
-    public void setSkyTime(String skyTime) { this.skyTime = skyTime; }
-
-    public String getAreaCd() { return areaCd; }
-
-    public void setAreaCd(String areaCd) { this.areaCd = areaCd; }
-
-    public String getAreaNm() { return areaNm; }
-
-    public void setAreaNm(String areaNm) { this.areaNm = areaNm; }
-
-    public String getWarngVarType() { return warngVarType; }
-
-    public void setWarngVarType(String warngVarType) { this.warngVarType = warngVarType; }
-
-    public String getWarngStressType() { return warngStressType; }
-
-    public void setWarngStressType(String warngStressType) { this.warngStressType = warngStressType; }
-
-    public String getCommandType() { return commandType; }
-
-    public void setCommandType(String commandType) { this.commandType = commandType; }
-
-    public String getStartTime() { return startTime; }
-
-    public void setStartTime(String startTime) { this.startTime = startTime; }
-
-    public String getStartTimeMd() {
-        return startTimeMd;
-    }
-
-    public void setStartTimeMd(String startTimeMd) {
-        this.startTimeMd = startTimeMd;
-    }
-
-    public String getStartTimeHm() {
-        return startTimeHm;
-    }
-
-    public void setStartTimeHm(String startTimeHm) {
-        this.startTimeHm = startTimeHm;
-    }
-
     public int getDeviceSnsrId() {
         return deviceSnsrId;
     }
@@ -215,4 +103,8 @@ public class weather {
     public void setDeviceSnsrId(int deviceSnsrId) {
         this.deviceSnsrId = deviceSnsrId;
     }
+
+    public String getAreaNm() { return areaNm; }
+
+    public void setAreaNm(String areaNm) { this.areaNm = areaNm; }
 }
--- src/main/java/api/weatherController.java
+++ src/main/java/api/weatherController.java
@@ -1,6 +1,7 @@
 package api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -10,6 +11,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -17,9 +19,226 @@ import java.util.List;
 @Service("WeatherController")
 public class weatherController {
 
+    @Value("${service.url}")
+    private String serviceUrl;
+
+    @Value("${service.key}")
+    private String serviceKey;
+
     public void getWeather() {
 
+        Calendar cal = Calendar.getInstance();
+
+        String baseDate = getBaseDate(cal.getTime(), "yyyyMMdd");
+        String baseTime = getBaseTime(cal, "HH", "WX_CURRENT");
+
+        List<weather> coordXyList = new ArrayList<>();
+        weather weatherLocationSet = new weather();
+        weatherLocationSet.setCoordX(60);     // 서울시 중구
+        weatherLocationSet.setCoordY(127);    // 서울시 중구
+        coordXyList.add(weatherLocationSet);
+        weatherLocationSet.setCoordX(61);     // 서울시 강남
+        weatherLocationSet.setCoordY(126);    // 서울시 강남
+        coordXyList.add(weatherLocationSet);
+        weatherLocationSet.setCoordX(99);     // 부산 해운대
+        weatherLocationSet.setCoordY(75);     // 부산 해운대
+        coordXyList.add(weatherLocationSet);
+
+        for(int i=0; i<coordXyList.size(); i++){
+
+            weatherVo weatherVo = getWeatherDataToVo(serviceUrl, serviceKey, baseDate, baseTime,
+                    coordXyList.get(i).getCoordX(), coordXyList.get(i).getCoordY());
+
+            if(weatherVo != null){
+
+                weather weather = parsingWeather(weatherVo);
+
+                if(weather != null) {
+
+                    System.out.println("지역명    : " + weather.getAreaNm() + "\n" +
+                                       "예보 시간 : " + weather.getBaseYmd() + " " + weather.getBaseTime() + "\n" +
+                                       "기온      : " + weather.getCurTemp() + "°C" + "\n" +
+                                       "습도      : " + weather.getHumidity() + "%" + "\n" +
+                                       "강수형태  : " + weather.getRainType() + "\n" +
+                                       "강수량    : " + weather.getRainProbabl() + "mm" + "\n" +
+                                       "풍속      : " + weather.getWind() + "m/s" + "\n" +
+                                       "풍향      : " + weather.getWindType() + "\n");
+
+                }
+
+            }
+
+        }
 
     }
+
+    public weatherVo getWeatherDataToVo(String serviceUrl, String serviceKey, String baseDate, String baseTime, int coordX, int coordY) {
+
+        weatherVo weatherVo = null;
+
+        try {
+
+            String reqUri = serviceUrl;
+            reqUri += "?serviceKey=" + serviceKey;
+            reqUri += "&base_date=" + baseDate;
+            reqUri += "&base_time=" + baseTime;
+            reqUri += "&nx=" + coordX;
+            reqUri += "&ny=" + coordY;
+            reqUri += "&numOfRows=100";
+            reqUri += "&pageNo=1";
+            reqUri += "&dataType=JSON";
+
+            String urlConResult = getUrlConResult(reqUri);
+
+            if (urlConResult != null) {
+                ObjectMapper objMapper = new ObjectMapper();
+                weatherVo = objMapper.readValue(urlConResult, weatherVo.class);
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return weatherVo;
+    }
+
+    public String getUrlConResult(String reqUri) {
+
+        String urlConResult = "";
+
+        try {
+
+            URL url = new URL(reqUri);
+            URLConnection urlConnection = url.openConnection();
+
+            urlConnection.setConnectTimeout(30000);
+            urlConnection.setReadTimeout(2000);
+
+            HttpURLConnection connection = null;
+            if (urlConnection instanceof HttpURLConnection) {
+                connection = (HttpURLConnection) urlConnection;
+            } else {
+                System.out.println("URL error");
+                return null;
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+
+            String current;
+            while ((current = br.readLine()) != null) {
+
+                urlConResult += current + "\n";
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return urlConResult;
+    }
+
+
+    public weather parsingWeather(weatherVo weatherVo) {
+
+        weather weather = null;
+
+        if ("00".equals(weatherVo.getResponse().getHeader().getResultCode())) {
+            weather = new weather();
+            List<weatherItemVo> item = weatherVo.getResponse().getBody().getItems().getItem();
+
+            if (item.size() > 0) {
+                if(60 == item.get(0).getNx() && 127 == item.get(0).getNy()){
+                    weather.setAreaNm("서울시 중구");
+                }else if(61 == item.get(0).getNx() && 126 == item.get(0).getNy()){
+                    weather.setAreaNm("서울시 강남");
+                }else if(99 == item.get(0).getNx() && 75 == item.get(0).getNy()){
+                    weather.setAreaNm("부산시 해운대");
+                }
+
+                weather.setBaseYmd(item.get(0).getBaseDate());
+                weather.setBaseTime(item.get(0).getBaseTime());
+
+                for(int i=0; i<item.size(); i++){
+
+                    if("T1H".equals(item.get(i).getCategory())){                // 기온
+                        weather.setCurTemp(item.get(i).getObsrValue());
+                    }else if("REH".equals(item.get(i).getCategory())){          // 습도
+                        weather.setHumidity(item.get(i).getObsrValue());
+                    }else if("PTY".equals(item.get(i).getCategory())){          // 강수형태
+                        if("0".equals(item.get(i).getObsrValue())){
+                            weather.setRainType("강수 없음");
+                        }else if("1".equals(item.get(i).getObsrValue())){
+                            weather.setRainType("비");
+                        }else if("2".equals(item.get(i).getObsrValue())){
+                            weather.setRainType("비/눈");
+                        }else if("3".equals(item.get(i).getObsrValue())){
+                            weather.setRainType("눈");
+                        }else if("4".equals(item.get(i).getObsrValue())){
+                            weather.setRainType("소나기");
+                        }
+                    }else if("RN1".equals(item.get(i).getCategory())){          // 1시간 강수량
+                        weather.setRainProbabl(item.get(i).getObsrValue());
+                    }else if("WSD".equals(item.get(i).getCategory())){          // 풍속
+                        weather.setWind(item.get(i).getObsrValue());
+                    }else if("VEC".equals(item.get(i).getCategory())){          // 풍향
+                        int windValue = Integer.parseInt(item.get(i).getObsrValue());
+                        String windType = "";
+                        // 범위별 구간 계산해서 표현 단위별 저장
+                        if(windValue >= 0 && windValue <= 44){
+                            windType = "N";
+                        }else if(windValue >= 45 && windValue <= 89){
+                            windType = "NE";
+                        }else if(windValue >= 90 && windValue <= 134){
+                            windType = "E";
+                        }else if(windValue >= 135 && windValue <= 179){
+                            windType = "SE";
+                        }else if(windValue >= 180 && windValue <= 224){
+                            windType = "S";
+                        }else if(windValue >= 225 && windValue <= 269){
+                            windType = "SW";
+                        }else if(windValue >= 270 && windValue <= 314){
+                            windType = "W";
+                        }else if(windValue >= 315 && windValue <= 360){
+                            windType = "NW";
+                        }else {
+                            windType = "-";
+                        }
+                        weather.setWindType(windType);
+                    }
+
+                }
+
+            }
+
+        }
+
+        return weather;
+    }
+
+    public String getBaseDate(Date date, String format) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+        String baseDate = simpleDateFormat.format(date);
+
+        return baseDate;
+    }
+
+    public String getBaseTime(Calendar cal, String format, String type) {
+
+        Date date = cal.getTime();
+        String baseTime = "";
+
+        int baseTimeMM = Integer.valueOf(getBaseDate(date, "mm"));
+        if(baseTimeMM < 40){
+            cal.add(Calendar.HOUR_OF_DAY, -1);
+            date = cal.getTime();
+        }
+        baseTime = getBaseDate(date, format) + "00";
+
+        return baseTime;
+    }
+
+
+
 
 }
